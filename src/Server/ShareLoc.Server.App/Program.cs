5d8c49f505611863a34f141d988ffc59bddd6f19
skrasek.michael <skrasek.michal@email.cz>
2023-10-11 12:09:43 +0200

    CI pipeline + move swagger to dev mode

--- src/Server/ShareLoc.Server.App/Program.cs
+++ src/Server/ShareLoc.Server.App/Program.cs
@@ -11,11 +11,13 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
 	app.UseSwagger();
 	app.UseSwaggerUI();
-
+}
+else
+{
 	app.UseExceptionHandler("/Error");
 	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 	app.UseHsts();
