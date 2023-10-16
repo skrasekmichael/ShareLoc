2dbda3867f821a9888da870c940b3d2c93a927ff
skrasek.michael <skrasek.michal@email.cz>
2023-10-16 22:27:51 +0200

    added DTOs and validators

---
+++ src/Shared/ShareLoc.Shared.Common/Models/CreatePlaceRequest.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+
+namespace ShareLoc.Shared.Common.Models;
+
+public sealed class CreatePlaceRequest
+{
+	public required double Latitude { get; init; }
+	public required double Longitude { get; init; }
+	public required string Message { get; init; }
+	public required byte[] Image { get; init; }
+}
+
+public sealed class CreatePlaceRequestValidator : AbstractValidator<CreatePlaceRequest>
+{
+	public CreatePlaceRequestValidator()
+	{
+		RuleFor(x => x.Latitude)
+			.InclusiveBetween(-90, 90)
+			.WithMessage("Latitude has to be number between -90 and 90.");
+
+		RuleFor(x => x.Longitude)
+			.InclusiveBetween(-180, 180)
+			.WithMessage("Longitude has to be number between -180 and 180.");
+
+		RuleFor(x => x.Message)
+			.MaximumLength(30)
+			.WithMessage("Message has maximum length of 30 characters.");
+	}
+}
---
+++ src/Shared/ShareLoc.Shared.Common/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using ShareLoc.Shared.Common.Models;
+
+namespace ShareLoc.Shared.Common;
+
+public static class ServiceCollectionExtensions
+{
+	public static IServiceCollection AddCommon(this IServiceCollection services)
+	{
+		services.AddScoped<IValidator<CreatePlaceRequest>, CreatePlaceRequestValidator>();
+		services.AddScoped<IValidator<CreateGuessRequest>, CreateGuessRequestValidator>();
+
+		return services;
+	}
+}
