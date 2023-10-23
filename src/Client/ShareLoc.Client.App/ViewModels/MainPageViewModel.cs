1ab7ea05eeacc66f129c4e8a1b63d38089607935
Dalibor Beneš <xbenes56@vutbr.cz>
2023-10-23 22:27:15 +0200

    client: intoduce view model for the main page

---
+++ src/Client/ShareLoc.Client.App/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+using CommunityToolkit.Maui.Core.Views;
+
+namespace ShareLoc.Client.App.ViewModels;
+internal class MainPageViewModel
+{
+	public ICommand SharePhotoAndLocation { get; }
+	public MainPageViewModel()
+	{
+		SharePhotoAndLocation = new Command(async () =>
+		{
+			if (!MediaPicker.IsCaptureSupported)
+			{
+				// Display error alert, abort
+				await Shell.Current.DisplayAlert("Error", "Camera not supported", "OK");
+				return;
+			}
+			// get photo using media picker
+			var photo = await MediaPicker.CapturePhotoAsync();
+
+			// get location using geolocation
+			var location = await Geolocation.GetLocationAsync();
+
+			// TODO: send photo and location to server using HttpClient
+			await Shell.Current.DisplayAlert("Success", $"Photo and location: {location.Latitude}, {location.Longitude} sent", "OK");
+		});
+	}
+
+}
