167541b5780cef15eee9a29560ee3e20512d5824
Michael Škrášek <skrasek.michal@email.cz>
2023-10-27 18:32:05 +0200

    Added guess result dto model
    * added guess result dto model
    
    * added distance to responses
    
    * added guesserId int createGuessRequest

--- src/Shared/ShareLoc.Shared.Common/Models/GuessRequest.cs
+++ src/Shared/ShareLoc.Shared.Common/Models/GuessRequest.cs
@@ -5,6 +5,7 @@ namespace ShareLoc.Shared.Common.Models;
 public sealed class GuessRequest
 {
 	public required Guid PlaceId { get; init; }
+	public required Guid GuesserId { get; init; }
 	public required double Latitude { get; init; }
 	public required double Longitude { get; init; }
 	public required string Name { get; init; }
