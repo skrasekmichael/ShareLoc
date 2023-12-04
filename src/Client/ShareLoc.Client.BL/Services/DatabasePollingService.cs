33182a1d01df4d8b75ee637850ae2623a13a825a
Daniel Moudrý <xmoudr01@stud.fit.vutbr.cz>
2023-12-04 21:01:55 +0100

    db polling service base

---
+++ src/Client/ShareLoc.Client.BL/Services/DatabasePollingService.cs
@@ -0,0 +1,31 @@
+﻿using System.Timers;
+
+namespace ShareLoc.Client.BL.Services;
+
+public class DatabasePollingService
+{
+	private readonly System.Timers.Timer _timer;
+
+	public DatabasePollingService()
+	{
+		_timer = new System.Timers.Timer(1000);
+		_timer.AutoReset = true;
+		_timer.Elapsed += TimeElapsedHandler;
+	}
+
+	public void Start()
+	{
+		PollForData();
+		StartTimer();
+	}
+	public void Stop() => StopTimer();
+
+	private void StartTimer() => _timer.Start();
+	private void StopTimer() => _timer?.Stop();
+
+	private void TimeElapsedHandler(object? sender, ElapsedEventArgs e) => PollForData();
+	private void PollForData()
+	{
+		// Poll for data
+	}
+}
