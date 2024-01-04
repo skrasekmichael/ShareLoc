f66cac46cb01e241d635fae5703b76ad8dd61f2e
Daniel Moudrý <xmoudr01@stud.fit.vutbr.cz>
2024-01-04 19:07:31 +0100

    Fixed scoring funtion, changed deleting old entries to a batch operation

--- src/Server/ShareLoc.Server.DAL/Repositories/PlaceRepository.cs
+++ src/Server/ShareLoc.Server.DAL/Repositories/PlaceRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.DocumentModel;
 
 using ShareLoc.Server.DAL.Entities;
 
@@ -13,4 +14,24 @@ public sealed class PlaceRepository : RepositoryBase
 
 	public Task<Place?> GetPlaceByIdAsync(Guid id, CancellationToken token = default) =>
 		_dbContext.LoadAsync<Place?>(id, token);
+
+	public Task DeletePlaceAsync(Place place, CancellationToken token = default) =>
+		_dbContext.DeleteAsync(place, token);
+
+	public Task<List<Place>> GetOldEntriesAsync(string isoTimeStampCutoff, CancellationToken token = default)
+	{
+		return _dbContext.ScanAsync<Place>(
+			new List<ScanCondition>
+			{
+				new("TimeStampUTC", ScanOperator.LessThanOrEqual, isoTimeStampCutoff)
+			}).GetRemainingAsync(token);
+	}
+
+	public Task DeletePlacesAsync(IEnumerable<Place> places, CancellationToken token = default)
+	{
+		var batch = _dbContext.CreateBatchWrite<Place>();
+		batch.AddDeleteItems(places);
+
+		return batch.ExecuteAsync(token);
+	}
 }
